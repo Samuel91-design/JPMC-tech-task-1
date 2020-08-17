91677c378fbfee0cd32ca8d6dfbb4df8c887855b
Samuel Ernest Toochukwu <samuelernest91@gmail.com>
2020-08-17 01:52:25 +0100

    Commit1
    Co-Authored-By: Samuel Ernest Toochukwu <69762450+samuel91-design@users.noreply.github.com>

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,12 +8,14 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
-  def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
-    quotes = [
-      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
-      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
-    ]
+    for qoute in quotes:
+      self.assertEqual(getDataPoint(qoute), (qoute['stock'], qoute['top_bid']['price'], qoute['top_ask']['price'], (qoute['top_bid']['price'] + qoute['top_ask']['price'])/2))
+      self.assertEqual(1,1)
+  #def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
+   # quotes = [
+    #  {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+     # {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    #]
     """ ------------ Add the assertion below ------------ """
 
 
